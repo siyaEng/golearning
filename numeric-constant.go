cf72d88194bd634206c520bea63b563f6f8f79cf
siya <laisiyuan@meimiao.net>
2017-10-27 11:04:27 +0800

    数值常量

--- numeric-constant.go
+++ numeric-constant.go
@@ -8,12 +8,10 @@ const (
 )
 
 func needInt(x int) int { return x*10 + 1 }
-func needFloat(x float64) float64 {
-	return x * 0.1
-}
+func needFloat(x float64) float64 { return x * 0.1}
 
 func main() {
 	fmt.Println(needInt(Small))
 	fmt.Println(needFloat(Small))
 	fmt.Println(needFloat(Big))
-}
+}//未指定的数据类型由上下文决定

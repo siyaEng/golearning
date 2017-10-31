cfbf7b68f09f41dd42cd1e253f7a3c55d8998804
siya <laisiyuan@meimiao.net>
2017-10-31 09:47:24 +0800

    指针

--- pointers.go
+++ pointers.go
@@ -1,3 +1,14 @@
+// Go 具有指针。指针保存了变量的内存地址
+// 类型 *T 是执行类型 T 的值的指针。其零值是 nil。
+// var p *int 定义一个指针 p
+// & 符号会生成一个指向其作用对象的指针
+// i := 42
+// p = &i
+// * 符号表示指针指向的底层的值。
+// fmt.Println(*p) // 通过指针 p 读取 i
+// *p = 21 // 通过指针 p 设置 i
+// 这也就是通常所说的 "间接饮用" 或 "或非直接饮用"。
+// 与 C 不同, Go 没有指针运算
 package main
 
 import "fmt"
@@ -5,12 +16,11 @@ import "fmt"
 func main() {
 	i, j := 42, 2701
 
-	p := &i // point to i
-	fmt.Println(*p) // read i through the pointer
-	*p = 21 // set i through the pointer
-	fmt.Println(i) // see the new value of i
+	p := &i // p 指向 i
+	fmt.Println(*p) // 读取 i 通过 指针 p
+	fmt.Println(i)
 
-	p = &j // point to j
-	*p = *p / 37 // divide j through the pointer
-	fmt.Println(j) // see the new value of j
+	p = &j // p 指向 j
+	*p = *p / 37 // 设置了 j 的值 通过指针 p
+	fmt.Println(j) 
 }

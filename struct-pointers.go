5a071464de647adbf586944656b3248fb7396971
siya <laisiyuan@meimiao.net>
2017-10-31 09:54:47 +0800

    结构体-指针

--- struct-pointers.go
+++ struct-pointers.go
@@ -1,5 +1,5 @@
-// 结构体字段可以通过结构体指针来访问
-// 通过指针间接的访问是透明的
+// 结构体字段可以通过结构体指针来访问。
+// 通过指针间接的访问是透明的。
 package main
 
 import "fmt"
@@ -11,7 +11,7 @@ type Vertex struct {
 
 func main() {
 	v := Vertex{1, 2}
-	p := &v
-	p.X = 1e9 //这里给结构体的第一个参数赋值了
+	p := &v // 指向 v
+	p.X = 1e9 // 设置 X 的值
 	fmt.Println(v)
 }

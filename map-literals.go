bb11e7c5940b56e4e9731214af61f87bb8e024eb
siya <laisiyuan@meimiao.net>
2017-11-06 16:02:47 +0800

    map文法

--- map-literals.go
+++ map-literals.go
@@ -3,21 +3,22 @@ package main
 
 import "fmt"
 
-type Test struct {
-	bob, pop int
+type Vertex struct {
+	Lat, Long float64
 }
 
-var m = map[string]Test{
-	"first": {
-		1, 2,
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{
+		40.68433, -74.39967,
 	},
-	"second": {
-		3, 4,
+	"Google": Vertex{
+		37.42202, -122.08408,
 	},
 }
 
 func main() {
-	for k, _ := range m {
-		fmt.Println(m[k])
+
+	for k, v := range m {
+		fmt.Printf("%v's postion is %v\n", k, v)
 	}
 }

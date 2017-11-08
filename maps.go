605d4460116e925ee286b3f51d3631f3d234efa6
siya <laisiyuan@meimiao.net>
2017-11-08 11:02:16 +0800

    map

--- maps.go
+++ maps.go
@@ -13,8 +13,7 @@ var m map[string]Vertex
 func main() {
 	m = make(map[string]Vertex)
 	m["Bell Labs"] = Vertex{
-		40.68433, -74.39947,
+		40.98433, -74.39967,
 	}
-
-	fmt.Println(m["Bell Labs"])
+	fmt.Println(m)
 }

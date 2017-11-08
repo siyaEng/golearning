8175070724372abde3aab30a8966c0f394157442
siya <laisiyuan@meimiao.net>
2017-11-08 11:22:28 +0800

    函数值

--- function-values.go
+++ function-values.go
@@ -9,18 +9,21 @@ import (
  	"math"
 )
 
+/**
+ * 函数参数
+ * fn func(float64, float64) float64 其中有两个函数参数，一个返回值
+ * fn 为传入的参数
+ */
 func compute(fn func(float64, float64) float64) float64 {
-	return fn(3, 4)
+	return fn(3, 4) 
 }
 
 func main() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
 	}
-
 	fmt.Println(hypot(5, 12))
 
 	fmt.Println(compute(hypot))
-	fmt.Println(math.Pow)
 	fmt.Println(compute(math.Pow))
 }
